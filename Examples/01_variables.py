58a62342b890cfecc9da69324f92cb310045dd00
Michael Pearson <mpearson2516@yahoo.com>
2021-05-17 08:37:49 -0700

    Added the first two example codes

---
+++ Examples/01_variables.py
@@ -0,0 +1,9 @@
+a = 25
+b = a
+
+print(a)
+print(b)
+
+b = 17
+print(a)
+print(b)
---
+++ Examples/02_string_formatting.py
@@ -0,0 +1,17 @@
+# name = "Bob"
+
+# print(f"Hello, {name}")
+
+# name = "Rolf"
+
+# print(f"Hello, {name}")
+
+# greeting = "Hello, {}"
+# with_name = greeting.format(name)
+# with_name_two = greeting.format('Rolf')
+# print(with_name)
+# print(with_name_two)
+
+longer_phrase = "Hello, {}. Today is {}."
+formatted = longer_phrase.format("Rolf", "Monday")
+print(formatted)
