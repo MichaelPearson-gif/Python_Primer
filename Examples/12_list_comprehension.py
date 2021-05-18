6b359c86f212772255074517b98a5468492ed30f
Michael Pearson <mpearson2516@yahoo.com>
2021-05-18 08:43:11 -0700

    Added some more lessons

---
+++ Examples/12_list_comprehension.py
@@ -0,0 +1,6 @@
+# numbers = [1, 3, 5]
+# doubled = [num*2 for num in numbers]
+
+friends = ["Rolf", "Sam", "Samantha", "Saurabh", "Jen"]
+starts_s = [friend for friend in friends if friend.startswith("S")]
+print(starts_s)
---
+++ Examples/destructuring_variables.py
@@ -0,0 +1,26 @@
+# x, y = 5, 11
+# t = 5, 11
+# x, y = t
+
+# print(x, y)
+
+# student_attendance = {
+#     "Rolf": 96,
+#     "Bob": 80,
+#     "Anne": 100
+# }
+
+# print(list(student_attendance.items()))
+
+# for t in student_attendance.items():
+#     print(t)
+#     # print(f"{student}: {attendence}")
+
+# people = [("Bob", 42, "Mechanic"), ("James", 24, "Artist"), ("Harry", 32, "Lecturer")]
+
+# for person in people:
+#     print(f"Name: {person[0]}, Age: {person[1]}, Profession: {person[2]}")
+
+*head, tail = [1, 2, 3, 4, 5]
+print(head)
+print(tail)
