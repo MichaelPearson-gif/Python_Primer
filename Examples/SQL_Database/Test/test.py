5ad0616e573050635cfb444ad75e0a502f71c13e
Michael Pearson <mpearson2516@yahoo.com>
2021-05-19 09:20:58 -0700

    Started the SQL Database section

---
+++ Examples/SQL_Database/Test/test.py
@@ -0,0 +1,29 @@
+import sqlite3
+# This is just a test file that shows how to run sql commands in Python
+from sqlite3.dbapi2 import Cursor
+
+connection = sqlite3.connect('data.db')
+
+cursor = connection.cursor()
+
+create_table = "CREATE TABLE users (id int, username text, password text)"
+cursor.execute(create_table)
+
+user = (1, 'jose', 'asdf')
+insert_query = "INSERT INTO users VALUES (?, ?, ?)"
+cursor.execute(insert_query, user)
+
+users = [
+    (2, 'rolf', 'asdf'),
+    (3, 'anne', 'xyz')
+]
+
+cursor.executemany(insert_query, users)
+
+select_query = "SELECT * FROM users"
+for row in cursor.execute(select_query):
+    print(row)
+
+connection.commit()
+
+connection.close()
