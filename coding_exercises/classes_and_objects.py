de8a64b6f2b0c5db14c5d0de35007f10d22e05f7
Michael Pearson <mpearson2516@yahoo.com>
2021-06-04 08:47:36 -0700

    finished part 36

---
+++ coding_exercises/classes_and_objects.py
@@ -0,0 +1,14 @@
+class Store:
+    def __init__(self, name):
+        self.name = name
+        self.items = []
+
+    def add_item(self, name, price):
+        item = {
+            'name': name,
+            'price': price
+        }
+        return self.items.append(item)
+
+    def stock_price(self):
+        return sum(item['price'] for item in self.items)
